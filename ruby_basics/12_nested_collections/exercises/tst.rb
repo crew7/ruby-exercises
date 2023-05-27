5c788165bf888b68603c1f4956e6d8872f6f44a6
crew7 <tyjesp@gmail.com>
2023-05-26 23:12:51 -0400

    Finished nested hash exercises

--- ruby_basics/12_nested_collections/exercises/tst.rb
+++
@@ -1,9 +0,0 @@
-abc = [
-    { name: 'Ruby', is_my_favorite?: true },
-    { name: 'JavaScript', is_my_favorite?: false },
-    { name: 'HTML', is_my_favorite?: false }
-]
-
-abc.each do |objects|
-    
-end
